644ef54a4bef64d4e88a78597ef212a79c68ee9d
amansohal8654 <amansohal8654@gmail.com>
2022-03-20 20:15:19 +1300

    almost done with everything going to deploy backend

--- pages/experiments/[formId].tsx
+++ pages/experiments/[formId].tsx
@@ -1,16 +1,22 @@
 import {useForm} from 'react-hook-form'
 import {zodResolver} from '@hookform/resolvers/zod'
-import {object, string, TypeOf, array} from "zod";
-import { useState } from 'react';
-import  Link from "next/link"
+import {z, object, string, TypeOf, array} from "zod";
+import { useState, useEffect } from 'react';
+import router from "next/router"
 import ExperimentData from "../../data"
+import axios from "axios"
 
+const createQuestionSchema = object({
+    question: string({
+        required_error: "question is required"
+    }),
+    type: z.enum(["Single", "Multi", "Optional"]),
+    options: string().array().optional(),
+}).array()
 
 const question = object({
-    ques : string().nonempty({
-        message: 'Question is required'
-    }),
-    answers : string().nonempty({
+    question : string(),
+    answer : string().nonempty({
         message: 'Answer is required'
     })
 });
@@ -28,22 +34,47 @@ const createFormSchema = object({
     phone: string().refine((val) => !Number.isNaN(parseInt(val, 10)), {
         message: "Expected number, received a string"
     }),
-    questions: array(question),
+    questionAnswer: array(question),
 })
 
 export type CreateFormInput = TypeOf< typeof createFormSchema>;
-
+type CreateQuestionInput = TypeOf<typeof createQuestionSchema>;
 function FormPage({query} : {query: any}){
     const data =  ExperimentData.find(value => value.experiment.id === query.formId);
-    const [questions, _] = useState(data);
-
-    const {register, handleSubmit, formState: { errors }} = useForm<CreateFormInput>({
+    const [questions, setQuestions] = useState<CreateQuestionInput>([]);
+    const [formSubmitError, setFormSubmitError] = useState<any>();
+    const {setValue, register, handleSubmit, formState: { errors }} = useForm<CreateFormInput>({
         resolver: zodResolver(createFormSchema),
     });
+    useEffect(() => {  
+        getExperiment();
+    }, [])
 
-    const onSubmit = (values : CreateFormInput) => {
-        console.log({values})
+    const getExperiment = async () => {
+        try{
+            const questions = await axios.get('http://localhost:3365/api/getQuestions/query.formId')
+            setQuestions(questions?.data);
+        }
+        catch(err){
+            console.log(err)
+        }
     }
+
+    const onSubmit = async (values : CreateFormInput) => {
+        try{
+            let formData : any = values
+            formData.phone = parseInt(formData.phone);
+            await axios.post('http://localhost:3365/api/formResponses',
+            formData
+            )
+            router.push('/experiments/thankyou')
+            console.log(formData)
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+    
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
@@ -59,28 +90,29 @@ function FormPage({query} : {query: any}){
                 </div>
                 <div className="form-element">
                     <label htmlFor="phone">Phone Number</label>
-                    <input id="phone" type="tel" placeholder="0123456789" {...register("phone")}/>
+                    <input id="phone" type="number" placeholder="0123456789" {...register("phone")}/>
                     {errors.phone ? <p>{errors.phone?.message}</p> : "" }
                 </div>
                 {
-                    questions?.experiment.questions.map((values, idx) =>
+                    questions?.map((values, idx) =>
                         <div className="form-element" key={idx}>
-                            <label htmlFor="phone">{values.Q}</label>
-                            {values.type == "Options" ? <div className="select">
-                            <select id="standard-select">
+                            <label id = {`questionAnswer.${idx}.question`}  htmlFor="question" {...register(`questionAnswer.${idx}.question`)}
+                            { ...setValue(`questionAnswer.${idx}.question`, values.question )}>{values.question}</label>
+                            {values.type == "Optional" ? <div className="select">
+                            <select id = {`question${idx}number`} {...register(`questionAnswer.${idx}.answer`)}>
                                 {values.options?.map((value : any, idx : number) => <option key = {idx} value={value}>{value}</option>)}
                             </select>
                             <span className="focus"></span>
                             </div> : ""}
-                            {values.type == "Single" ? <input id="Ans" type="string" placeholder="Answer Please" /> : ""}
+                            {values.type == "Single" ? <input id={`questions.${idx}.answer`} type="string" placeholder="Answer Please" {...register(`questionAnswer.${idx}.answer`)} /> : ""}
                             {values.type == "Multi" ? 
-                            <textarea rows = {5} cols = {60} name = "description" defaultValue = "Enter details here..."/> : ""}
+                            <textarea id={`questions.${idx}.answer`} rows = {5} cols = {60} placeholder = "Enter details here..." {...register(`questionAnswer.${idx}.answer`)}/> : ""}
+                            {errors.questionAnswer ? <p>{errors.questionAnswer[idx]?.answer?.message}</p> : "" }
                         </div>
                     )
                 }
-                <Link href = "/experiments/thankyou">
-                    <button type= "submit">SUBMIT</button>
-                </Link>
+                <button type= "submit">SUBMIT</button>
+                {formSubmitError && formSubmitError}
             </form>
         </>
     );
--- pages/experiments/list.tsx
+++ pages/experiments/list.tsx
@@ -1,9 +1,45 @@
-import ExperimentData from "../../data"
 import Link from 'next/link'
+import axios from 'axios'
+import { useEffect, useState} from 'react'
+import {z, object, string, TypeOf, array, boolean} from "zod";
 
+const questionAnswer = object({
+    question: string({
+        required_error: "question is required"
+    }),
+    type: z.enum(["Single", "Multi", "Optional"]),
+    options: string().array().optional(),
+})
 
+const createExperimentSchema =  object({
+        _id: string(),
+        name: string({
+             required_error: "name is required"
+        }),
+        isActive: boolean({
+            required_error: "isActive is required",
+            invalid_type_error: "isActive must be a boolean",
+        }),
+        questions: array(questionAnswer)
+    }).array()
+
+export type CreateExperimentInput = TypeOf< typeof createExperimentSchema>;
 function ExperimentList(){
-    debugger;
+    const [experimentData, setExperimentData] = useState<CreateExperimentInput>([]);
+    useEffect(() => {  
+        getExperiment();
+    }, [])
+
+    const getExperiment = async () => {
+        try{
+            const experiments = await axios.get('http://localhost:3365/api/getExperiments')
+            setExperimentData(experiments?.data);
+            console.log(experiments);
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
     return (
         <>
             <div className="main-container">
@@ -12,12 +48,12 @@ function ExperimentList(){
                 </div>
                 <div className="cards">
                     {
-                        ExperimentData.map((value, index) => (
-                            <Link href={`/experiments/${value.experiment.id}`} key = {index}>
+                        experimentData?.map((value, index) => (
+                            <Link href={`/experiments/${value?._id}`} key = {index}>
                                 <div className="card card-1">
                                     <div className="card__icon"><i className="fas fa-bolt"></i></div>
                                     <p className="card__exit"><i className="fas fa-times"></i></p>
-                                    <h2 className="card__title">{value.experiment.name}</h2>
+                                    <h2 className="card__title">{value.name}</h2>
                                 </div>
                             </Link>
                         ))
